63526dfac1530b88770d96961ada7e41e88f68b3
Lourenço Mestre <1220766@isep.ipp.pt>
2023-10-29 12:28:54 +0000

    US06 almost finished

---
+++ BDDAD/US06/US06Script.sql
@@ -0,0 +1,15 @@
+SELECT
+    AP.parcelDesignation AS "Parcela",
+    FT.formulationTypeName AS "Tipo de Fator",
+    COUNT(DISTINCT P.productID) AS "Número de Fatores de Produção Aplicados"
+FROM
+    AgriculturalParcel AP
+        JOIN Plantation PL ON AP.parcelID = PL.AgriculturalParcelparcelID
+        JOIN Operation OP ON PL.cycleID = OP.PlantationCyclecycleID
+        JOIN Product_Operation PO ON OP.operationID = PO.OperationoperationID
+        JOIN Product P ON PO.ProductproductID = P.productID
+        JOIN FormulationType FT ON P.FormulationTypeformulationTypeID = FT.formulationTypeID
+WHERE
+        OP."date" >= TO_DATE('06-10-2016','DD-MM-YYYY') AND OP."date" <= TO_DATE('29-10-2023','DD-MM-YYYY')
+GROUP BY
+    AP.parcelDesignation, FT.formulationTypeName;
