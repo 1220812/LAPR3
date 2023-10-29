0cc8bf25316980fe4bf02831f80670de0d307ae1
Rodrigo Brito <1220842@isep.ipp.pt>
2023-10-29 17:11:42 +0000

    US BD8

--- BDDAD/US08/US08Script.sql
+++ BDDAD/US08/US08Script.sql
@@ -13,26 +13,3 @@ GROUP BY
     OT.operationTypeName
 ORDER BY
     Numero_De_Aplicacoes DESC;
-
-
-=======
-
-SELECT
-    p.comercialName AS "Product name",
-    p.productID AS "Product ID",
-    COUNT(po.OperationoperationID) AS "Number of applications"
-FROM
-    Product p
-        LEFT JOIN
-            Product_Operation po ON p.productID = po.ProductproductID
-        LEFT JOIN
-            Operation o ON po.OperationoperationID = o.operationID
-WHERE
-    o."date" BETWEEN TO_DATE('06-10-2016', 'DD-MM-YYYY') AND TO_DATE('29-10-2023', 'DD-MM-YYYY')
-GROUP BY
-    p.comercialName, p.productID
-ORDER BY
-    "Number of applications" DESC
-FETCH FIRST 1 ROW ONLY;
->>>>>>> origin/master
-
