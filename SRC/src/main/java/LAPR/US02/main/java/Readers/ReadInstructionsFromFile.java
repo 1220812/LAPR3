8c1c9da55176c4c50e6ee2c8dbebc5fb4c4b57f0
Francisco Silveira <1220813@isep.ipp.pt>
2023-11-25 22:20:57 +0000

    Update instructions.txt Update IrrigationUI Update Main Update ReadInstructionsFromFile

--- SRC/src/main/java/LAPR/US02/main/java/Readers/ReadInstructionsFromFile.java
+++ SRC/src/main/java/LAPR/US02/main/java/Readers/ReadInstructionsFromFile.java
@@ -5,6 +5,8 @@ import LAPR.US02.main.java.Domain.Partition;
 import LAPR.US02.main.java.Domain.ResultEntry;
 import LAPR.US02.main.java.Domain.SprinklingSystem;
 
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -12,14 +14,19 @@ import java.util.Scanner;
 
 public class ReadInstructionsFromFile {
     public List<ResultEntry> readInformation(String fileName) throws IOException {
-        Scanner sc = new Scanner(fileName);
+        File file = new File(fileName);
+        Scanner sc = new Scanner(file);
         String[] header = sc.nextLine().split(",");
         List<Partition> result = new ArrayList<>();
+
         while (sc.hasNextLine()) {
             String[] line = sc.nextLine().split(",");
             result.add(new Partition(line[0], Integer.parseInt(line[1]), line[2]));
         }
+
         SprinklingSystem sprksis = new SprinklingSystem(result);
-        return sprksis.generateSimulation(30, header[0], header[1]);
+        List<ResultEntry> resultEntries = sprksis.generateSimulation(30, header[0], header[1]);
+
+        return resultEntries;
     }
 }
