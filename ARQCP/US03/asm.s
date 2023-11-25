f5b123dcf45e0ecc6fae5b2665012f6d7cda6fd4
Francisco Silveira <1220813@isep.ipp.pt>
2023-11-25 23:41:46 +0000

    US03 ARQCP

--- ARQCP/US03/asm.s
+++ ARQCP/US03/asm.s
@@ -3,7 +3,6 @@
 .section .text
 
     .global move_num_vec
-
     # Arguments:
     # rdi -> int* array
     # rsi -> int length
@@ -12,10 +11,6 @@
     # r8  -> int num
     # r9  -> int* vec
 
-
-#-------------------------BEGINING--------------------------------------
-
-
 move_num_vec:
 
     pushq %rbx
@@ -23,55 +18,46 @@ move_num_vec:
     movq %rsp, %rbp
 
     cmpq %r8, %rbx            # Compare with the requested number to move
-
-		#IF R8 < RBX
-		jl not_enough_elements    # Jump if not enough elements
+    jl not_enough_elements    # Jump if not enough elements
 
     movq %r9, %r11           # Store the destination address in %r11
 
     movq $0, %rax            # use %rax as a counter start it at 0
 
-
-#--------------------------START COPY-----------------------------------
-
-copy:
-
+can_copy:
      movslq (%rdx, %rax, 4), %r9   # Move the 32-bit value to the destination
-     movq %r9, (%r11)
 
-     addq $1, %rax                 # add 1 to the counter
-     addq $4, %r11                 # adds 4 bits to the vec to pass to the next positon
+     cmpq $0, %r9               # Compare whats in %r9 with 0
+     je reset_reader            # If equal jump to reset_reader
 
-     decq %r8                      # decrement num
+     movl $0, (%rdx, %rax, 4)   # Move 0 to the position in the first array to "delete" it
+     movq %r9, (%r11)           # Move whats in %r9 to (%R11) to the new array
 
-		#IF NUM = 0
-		jz enough_elements            # jumps to enough_elements
+     addq $1, %rax             # add 1 to the counter
+     addq $4, %r11             # adds 4 bits to the vec to pass to the next positon
+
+     decq %r8                  # decrement num
+     jz enough_elements        # if num = 0 jumps to enough_elements
 
 
-     jmp copy              # jump to the loop again
+     jmp can_copy              # jump to the loop again
 
-
-#-------------------ENOUGHT ELEMENTS IN ARRAY---------------------------
+reset_reader:
+    subq $1, %rax              # Subtract 1 from %rax
+    subl $1, %esi              # subtract 1 from %esi the length
+    cmpl $0, %esi              # compare if length is 0
+    je can_copy                # if it equals 0 jump to can_copy
+    jmp reset_reader           # Jump again to reser_reader
 
 
 enough_elements:
-
     movq $1, %rax                 # Set return value to 1
-    jmp final                      # Jump to done
-
-
-#------------------NOT ENOUGH ELEMENTS IN ARRAY-------------------------
-
+    jmp done                      # Jump to done
 
 not_enough_elements:
-
     movq $0, %rax                 # Set return value to 0
 
-
-#------------------------FINAL------------------------------------------
-
-final:
-
+done:
     popq %rbp                     # Restore registers
     popq %rbx                     # Restore old value %rdx
 
