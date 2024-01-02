77c26f9e42c7703dd4b51eab42bb370d0cfed72e
Francisco Silveira <1220813@isep.ipp.pt>
2024-01-02 15:10:06 +0000

    US26 BASE DE DADOS

--- BDDAD/US26/US26Script.sql
+++ BDDAD/US26/US26Script.sql
@@ -1,25 +1,33 @@
-CREATE OR REPLACE TRIGGER Tgr_Create_Log
-AFTER INSERT OR UPDATE ON Operation
+CREATE OR REPLACE TRIGGER trg_registar_operacao_log
+AFTER INSERT ON operation
 FOR EACH ROW
 DECLARE
     Id NUMBER(10);
 BEGIN
-    SELECT NVL(MAX(Log.logID) + 1, 1)
+    SELECT NVL(MAX(log.logID) + 1, 1)
     INTO Id
-    FROM Log;
+    FROM log;
 
-    IF INSERTING THEN
-        INSERT INTO Log (logID, instant, operationID)
+    INSERT INTO log (logID, instant, operationID)
+    VALUES (Id, SYSTIMESTAMP, :NEW.operationID);
+END trg_registar_operacao_log;
+/
+
+------------------------------------------------------------------------------------------------------------------------------
+
+CREATE OR REPLACE TRIGGER trg_registar_mudanca_operacao_log
+AFTER UPDATE ON operation
+FOR EACH ROW
+DECLARE
+    Id NUMBER(10);
+BEGIN
+    IF :NEW.operationState <> :OLD.operationState THEN
+        SELECT NVL(MAX(log.logID) + 1, 1)
+        INTO Id
+        FROM log;
+
+        INSERT INTO log (logID, instant, operationID)
         VALUES (Id, SYSTIMESTAMP, :NEW.operationID);
-    ELSIF UPDATING THEN
-        INSERT INTO Log (logID, instant, operationID)
-        VALUES (Id, SYSTIMESTAMP, :OLD.operationID);
     END IF;
-
-      COMMIT;
-EXCEPTION
-    WHEN OTHERS THEN
-        ROLLBACK;
-        RAISE;
-END;
+END trg_registar_mudanca_operacao_log;
 /
