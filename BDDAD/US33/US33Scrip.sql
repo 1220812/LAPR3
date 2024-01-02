2a3af1408dd84ef9a83258912a4ff928bc60b76a
Francisco Silveira <1220813@isep.ipp.pt>
2024-01-02 05:10:35 +0000

    US3 BASE DE DADOS

--- BDDAD/US33/US33Scrip.sql
+++ BDDAD/US33/US33Scrip.sql
@@ -1,25 +1,28 @@
 WITH TotalConsumo AS (
-    SELECT C.cropID,
-           SUM(W.duration) AS total_consumo_agua
-FROM
-    Watering W
-JOIN
-    Operation O ON O.operationID = W.operationID
-JOIN
-    Crop C ON C.cropID = W.cropID
-WHERE
-    EXTRACT(YEAR FROM operationDate) = 2023
-GROUP BY
-    C.cropID),
- MaxConsumo AS (
-SELECT
-    MAX(total_consumo_agua) AS max_consumo
-FROM
-    TotalConsumo
+    SELECT
+        C.cropID,
+        SUM(W.duration) AS total_consumo_agua
+    FROM
+        Watering W
+    JOIN
+        Operation O ON O.operationID = W.operationID
+    JOIN
+        Crop C ON C.cropID = O.cropID
+    WHERE
+        EXTRACT(YEAR FROM O.operationDate) = 2023
+    GROUP BY
+        C.cropID
+),
+MaxConsumo AS (
+    SELECT
+        MAX(total_consumo_agua) AS max_consumo
+    FROM
+        TotalConsumo
 )
 SELECT
-    cropID, total_consumo_agua
+    TC.cropID,
+    TC.total_consumo_agua
 FROM
-    TotalConsumo
-WHERE
-    total_consumo_agua = (SELECT max_consumo FROM MaxConsumo);
+    TotalConsumo TC
+JOIN
+    MaxConsumo MC ON TC.total_consumo_agua = MC.max_consumo;
