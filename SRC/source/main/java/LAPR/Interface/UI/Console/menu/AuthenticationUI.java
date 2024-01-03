61d026652931daf0b757ed64cbb031c0d682f072
Francisco Silveira <1220813@isep.ipp.pt>
2024-01-03 21:13:48 +0000

    Interface LAPR3

--- SRC/source/main/java/LAPR/Interface/UI/Console/menu/AuthenticationUI.java
+++ SRC/source/main/java/LAPR/Interface/UI/Console/menu/AuthenticationUI.java
@@ -1,18 +1,25 @@
 package LAPR.Interface.UI.Console.menu;
 
-
 import LAPR.Interface.UI.Console.Utils.Utils;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class AuthenticationUI implements Runnable {
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_CYAN = "\u001B[36m";
+    public static final String ANSI_BOLD = "\u001B[1m";
 
     @Override
     public void run() {
-        List<MenuItem> authenticationOption = new ArrayList<MenuItem>();
-        authenticationOption.add(new MenuItem("Product Owner", new ProductOwnerUI()));
-        authenticationOption.add(new MenuItem("Gestor Agricola", new GestorAgricolaUI()));
+        List<MenuItem> authenticationOption = new ArrayList<>();
+        System.out.println(ANSI_CYAN + "\n=============================================");
+        System.out.println("          Authentication Options             ");
+        System.out.println("=============================================" + ANSI_RESET);
+
+        authenticationOption.add(new MenuItem(ANSI_GREEN + ANSI_BOLD + "Product Owner" + ANSI_RESET, new ProductOwnerUI()));
+        authenticationOption.add(new MenuItem(ANSI_GREEN + ANSI_BOLD + "Gestor Agricola" + ANSI_RESET, new GestorAgricolaUI()));
 
         int option = 0;
         do {
@@ -23,6 +30,4 @@ public class AuthenticationUI implements Runnable {
             }
         } while (option != -1);
     }
-
 }
-
